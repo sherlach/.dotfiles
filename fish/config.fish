363a23b0726c847d8f6f45f0e83533a7f9adf3c6
sherlach <68379649+sherlach@users.noreply.github.com>
2021-08-18 18:58:34 +1000

    tuning fish git prompt

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,28 @@
-set -U EDITOR vise
+set -U EDITOR vim
 set -U --erase fish_greeting
+
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+#set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_color_branch magenta 
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+#set -g __fish_git_prompt_color_cleanstate green bold
+set -g __fish_git_prompt_color_cleanstate green
 
 set -agx PATH "$HOME/go/bin"
 
